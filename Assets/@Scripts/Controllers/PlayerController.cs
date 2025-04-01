79e44a43ec97394854d0c3b9c5e64e7d3229b13b
Woosung <lwwl9738@gmail.com>
2025-04-01 18:22:41 +0900

    Add GameScene and PlayerController cl2

---
+++ Assets/@Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 _moveDir = Vector2.zero;
+    float _speed = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdateInput();
+        MovePlayer();
+    }
+
+
+    //DeviceSimulator에서는 먹통이다 핸드폰에는 키보드가 없으니까
+    void UpdateInput() //키보드 입력을 받아서 Dir을 바꿔주는 함수
+    {
+        Vector2 moveDir = Vector2.zero;
+
+        //if else가 아니니까 동시에 적용이 가능하다!
+        if (Input.GetKey(KeyCode.W))
+            moveDir.y += 1;
+        if (Input.GetKey(KeyCode.S))
+            moveDir.y -= 1;
+        if (Input.GetKey(KeyCode.A))
+            moveDir.x -= 1;
+        if (Input.GetKey(KeyCode.D))
+            moveDir.x += 1;
+
+        _moveDir = moveDir.normalized;
+    }//애는 어차피 나중에 UI로 대체
+
+
+    void MovePlayer()
+    {
+        Vector3 dir = _moveDir * _speed *Time.deltaTime;
+        transform.position += dir;
+    }
+}
